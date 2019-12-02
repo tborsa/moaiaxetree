c8740987f8300f7bfbceccd6929e455c1f2c6e0d
andydlindsay@gmail.com <andydlindsay@gmail.com>
2019-12-01 23:12:25 -0800

    set project to initial state

---
+++ moaiAxeTree/src/helpers/helpers.test.js
@@ -0,0 +1,25 @@
+import { announceResult } from './helpers'
+
+describe('announceResult function', () => {
+  test('returns "Won" if player is "Axe" and comp is "Tree"', () => {
+    const playerSelection = 'Axe';
+    const compSelection = 'Tree';
+    expect(announceResult(playerSelection, compSelection)).toBe('Won');
+  });
+
+  test('returns "Tied" if player is "Axe" and comp is "Axe"', () => {
+    const playerSelection = 'Axe';
+    const compSelection = 'Axe';
+    expect(announceResult(playerSelection, compSelection)).toBe('Tied');
+  });
+
+  test('returns "Lost" if player is "Axe" and comp is "Moai"', () => {
+    const playerSelection = 'Axe';
+    const compSelection = 'Moai';
+    expect(announceResult(playerSelection, compSelection)).toBe('Lost');
+  });
+
+  test('returns "Waiting" if nothing is passed in', () => {
+    expect(announceResult()).toBe('Waiting');
+  });
+});
